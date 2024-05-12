14cdf3c7b5b7a8b2fdddbf7ff643e6d6abf18151
Alix2019 <alix1982@yandex.ru>
2024-05-12 19:35:40 +0300

    fix_backend_deploy5

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -5,25 +5,46 @@ require('dotenv').config();
 const { SMTP_HOST, SMTP_PORT, SMTP_SECURE, SMTP_MAIL, SMTP_APP_PASS } = process.env;
 
 const sendEmail = async (options) => {
+  // const transporter = nodeMailer.createTransport({
+  //   host: SMTP_HOST,
+  //   port: SMTP_PORT,
+  //   secure: SMTP_SECURE,
+  //   auth: {
+  //     user: SMTP_MAIL,
+  //     pass: SMTP_APP_PASS,
+  //   },
+  // });
+
+  // const mailOptions = {
+  //   from: SMTP_MAIL,
+  //   to: options.to,
+  //   subject: options.subject,
+  //   html: options.html,
+  // };
+
+  // SMTP_SECURE=true
+
   const transporter = nodeMailer.createTransport({
-    host: SMTP_HOST,
-    port: SMTP_PORT,
-    secure: SMTP_SECURE,
+    host: 'smtp.yandex.ru',
+    port: 465,
+    // secure: SMTP_SECURE,
     auth: {
-      user: SMTP_MAIL,
-      pass: SMTP_APP_PASS,
+      user: 'testnickulin@yandex.ru',
+      pass: 'kgxnmxcesucxqljy',
     },
   });
 
   const mailOptions = {
-    from: SMTP_MAIL,
-    to: options.to,
-    subject: options.subject,
-    html: options.html,
+    from: 'testnickulin@yandex.ru',
+    to: 'alix1982@yandex.ru',
+    subject: 'Вопрос',
+    html: `
+    <h1>Имя: dfgdfgdf</h1>
+  `,
   };
 
   await transporter.sendMail(mailOptions);
-  await console.log('Email отправлен');
+  console.log('Email отправлен');
 };
 
 module.exports = sendEmail;
